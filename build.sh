d3c22501d4c836f002f1e0e97ae285a00a60d6ec
Anthony Porthouse <anthony.porthouse@bbc.co.uk>
2024-07-02 11:11:22 +0100

    feat: extract packwiz-renderer

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #! /usr/bin/env bash
 
-curl -Lo "packwiz.zip" "https://nightly.link/packwiz/packwiz/workflows/go/main/Linux%2064-bit%20x86.zip"
+if [[ $(uname) = "Darwin" ]] && [[ $(uname -m) = "arm64" ]]; then
+    curl -Lo "packwiz.zip" "https://nightly.link/packwiz/packwiz/workflows/go/main/macOS%2064-bit%20ARM.zip"
+elif [[ $(uname) = "Darwin" ]] && [[ $(uname -m) = "x86_64" ]]; then
+    curl -Lo "packwiz.zip" "https://nightly.link/packwiz/packwiz/workflows/go/main/macOS%2064-bit%20x86.zip"
+elif [[ $(uname) = "Linux" ]] && [[ $(uname -m) = "arm64" ]]; then
+    curl -Lo "packwiz.zip" "https://nightly.link/packwiz/packwiz/workflows/go/main/Linux%2064-bit%20ARM.zip"
+elif [[ $(uname) = "Linux" ]] && [[ $(uname -m) = "x86_64" ]]; then
+    curl -Lo "packwiz.zip" "https://nightly.link/packwiz/packwiz/workflows/go/main/Linux%2064-bit%20x86.zip"
+fi
 
 unzip -u packwiz.zip
 
@@ -12,8 +20,6 @@ cd pack || exit 1
 
 cd - || exit 1
 
-npm i
-
-npx tsx bin/build.ts
+npx -y github:anthonyporthouse/packwiz-renderer build pack --output public
 
 cp -r pack/* public
